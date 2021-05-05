a5c7a16d009dd19e6b0a5c7e375436a3c1464320
Hugo Simoes <hugolfsimoes@gmail.com>
2021-05-05 13:05:08 -0300

    feito a criacao de forms e um componente de lista

---
+++ ceep/src/components/ListaDeNotas.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+class ListaDeNotas extends React.Component {
+
+  render() {
+    return(
+      <ul>
+        <li>
+        <section>
+          <header>
+            <h3>Título</h3>
+          </header>
+          <p>Escreva sua nota...</p>
+        </section>
+      </li>
+      <li>
+        <section>
+          <header>
+            <h3>Título</h3>
+          </header>
+          <p>Escreva sua nota...</p>
+        </section>
+      </li>
+      <li>
+        <section>
+          <header>
+            <h3>Título</h3>
+          </header>
+          <p>Escreva sua nota...</p>
+        </section>
+      </li>
+    </ul>
+    );
+  }
+}
+
+export default ListaDeNotas;
